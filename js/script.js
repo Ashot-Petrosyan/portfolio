819f602b4e531fc3addd3e5d7b32a53f0a59c15b
Ashot-Petrosyan <61912592+Ashot-Petrosyan@users.noreply.github.com>
2020-04-16 14:27:21 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+	$('.click').click(function() {
+		$(".hide_skills").addClass("shows_skilss");
+		$(".html").addClass('skills_100');
+		$(".css").addClass('skills_100');
+		$(".js").addClass('skills_100');
+		$(".bootstrap").addClass('skills_80');
+		$(".jquery").addClass('skills_80');
+		$(".react").addClass('skills_20');
+
+	});
+});
